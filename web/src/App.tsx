a8bc0a6949131d9bc6d255b46d994831f06aeacf
Gabriel Reira <62738460+GabrielReira@users.noreply.github.com>
2020-08-03 20:06:58 -0300

    Finalizada a landing page

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Landing from './pages/Landing/index'
+
+import './assets/styles/global.css';
+
+// JSX = JavaScript + XML = HTML dentro do JS
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
