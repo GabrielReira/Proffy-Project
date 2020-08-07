85fccb5e9d02a654d597ac6f023a290a1f18e49d
Gabriel Reira <gabrielmfreira@gmail.com>
2020-08-07 19:03:09 -0300

    Finalizado o filtro de professores

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -7,7 +7,50 @@ const styles = StyleSheet.create({
     },
 
     teacherList: {
-        marginTop: -60
+        marginTop: -50
+    },
+
+    searchForm: {
+        marginBottom: 20
+    },
+
+    label: {
+        color: '#d4c2ff',
+        fontFamily: 'Poppins_400Regular'
+    },
+
+    input: {
+        height: 44,
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        marginTop: 4,
+        marginBottom: 16
+    },
+
+    inputGroup: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    inputBlock: {
+        width: '48%'
+    },
+
+    submitButton: {
+        backgroundColor: '#04d361',
+        flexDirection: 'row',
+        height: 46,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    submitButtonText: {
+        color: '#fff',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16
     }
 })
 
